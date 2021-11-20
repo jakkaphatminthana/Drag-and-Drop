2ee18f510a9a84badef0e705f91e404067533bca
ChampJM <58208814+jakkaphatminthana@users.noreply.github.com>
2021-11-20 21:12:13 +0700

    Add files via upload

--- control.js
+++ control.js
@@ -1,47 +1,42 @@
 const BOX = document.querySelectorAll(".box")
 const Box_img = document.querySelectorAll(".box img")
 const PICTURE = document.querySelectorAll('.box-form')
-const Conti = document.querySelectorAll('.container-right')
 const GroupBox = document.querySelectorAll('.group-box')
 
 const BoxChingSize = () => {
+    
+    if(data.length !== 28) return alert("ยังออกเเบบยังไม่เสร็จ") 
     BOX.forEach((e) => {
-        e.style.margin = "-15px"
+        e.style.marginLeft = "-20px"
+        e.style.marginRight = "-20px"
+        e.style.marginTop = "-25px"
+        e.style.marginBottom = "-25px"
     })
     Boxdef()
     GroupBoxDef()
     Box_img_Def()
+    
 }
 
 const Boxdef = () => {
 
     PICTURE.forEach((e) => {
-        e.style.height = "82%"
-        e.style.width = " 91%"
+        e.style.height = "75%"
+        e.style.width = " 70%"
 
     })
 }
 
 const GroupBoxDef = () => {
-
     GroupBox.forEach((e) => {
-        e.style.marginTop = "-730px";
+        e.style.marginTop = "-660px";
     })
 }
 
 const Box_img_Def = () => {
-
     Box_img.forEach((e) => {
         e.style.backgroundColor = "transparent";
         e.style.height = "95px";
         e.style.width = "95px"
     })
 }
-
-
-/*
-width: 91%;
-height: 82%;
-*/
-
-/*e.style.backgroundColor = "transparent";*/
